58f64c20d44e2ede7a56f87805e87b6b94098388
dbdnrl123 <dbdnrl123@gmail.com>
2022-08-24 17:11:37 +0900

    실습 보고서

---
+++ Ch01/src/sbu1/HelloWorld.java
@@ -0,0 +1,26 @@
+package sbu1;
+
+/*
+ * 날짜 : 2022/08/16s
+ * 이름 : 양재영
+ * 내용 : Java 개발환경 구축 실습
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		// 기본출력
+		System.out.println("Hello World!");
+		System.out.println("Hello Java!");
+		System.out.println("hello");
+		
+		// 한줄출력
+		System.out.print("Hello ");
+		System.out.print("Korea!");
+		
+		// 개행처리
+		System.out.println();
+		System.out.println("Hello Busan!");
+	}
+}
+
---
+++ Ch03/src/sub2/SwitchTest.java
@@ -0,0 +1,22 @@
+package sub2;
+
+import java.util.Scanner;
+
+/*
+ * 날짜 : 2022/08/17
+ * 이름 : 양재영
+ * 내용 : Java 조건문 switch 실습하기
+ */
+public class SwitchTest {
+	public static void main(String[] args) {
+		
+		// 사용자 입력
+		System.out.print("숫자 입력 :");
+		
+		Scanner scan = new Scanner(System.in);
+		int number = scan.nextInt();
+		
+		System.out.println("입력한 숫자 : "+number);
+		
+	} 
+}
